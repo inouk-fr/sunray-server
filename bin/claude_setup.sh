101cef7c34bee63dfb38bf582135b5194e234798
Cyril MORISSE <cmorisse@boxes3.net>
2025-08-08 18:23:48 +0200

    Claude setup.sh

--- bin/claude_setup.sh
+++ bin/claude_setup.sh
@@ -16,3 +16,14 @@ echo "npm version     : $(npm -v)"
 # Install Claude code
 #
 sudo npm install -g @anthropic-ai/claude-code
+#
+# Install playwright
+# Ref: https://community.nodebb.org/topic/e118d50c-ab30-47a9-b444-7779569e48c4/i-figured-out-how-to-add-the-official-playwright-browser-automation-mcp-to-claude-code.
+sudo apt install -y chromium
+claude mcp add playwright npx '@playwright/mcp@latest'
+
+#
+# Use playwright
+# Ref: https://til.simonwillison.net/claude-code/playwright-mcp-claude-code
+# in claude code prompt:
+#  Use playwright mcp to open a browser at https://www.muppy.io
