728f1683b1f8c40dc3e9002e20ba844491535547
Mathias MANGON <mangon.m@gmail.com>
2025-08-22 14:37:25 +0200

    IMP: better log in sunray init db

--- bin/sunray_init_db.sh
+++ bin/sunray_init_db.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "Launching Database "
-echo "  /opt/muppy/appserver-sunray18/bin/mpy_init_db.sh"
+
+# Function to log with timestamp and step info
+log_step() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] SUNRAY-INIT: $1"
+}
+
+log_step "Starting Sunray database initialization"
+log_step "Script: /opt/muppy/appserver-sunray18/bin/sunray_init_db.sh"
 echo -e "\n"
+
+log_step "Installing sunray_core module"
 /opt/muppy/appserver-sunray18/bin/sunray-srvr -i sunray_core  --without-demo=all --stop-after-init
 
 #
@@ -34,28 +42,47 @@ echo -e "\n"
 # - IKB_SMTP_USER
 # - IKB_SMTP_PASSWORD
 # - IKB_EMAIL_FROM
+#
+# New environment variables for company setup:
+# - MPY_USERINIT_WEBSITE: Website URL for company (default: https://gitlab.com/cmorisse/inouk-sunray-server)
+# - MPY_USERINIT_PARTNER_ROOT_NAME: Partner root name (default: SunrayBot)
 
+log_step "Setting up company configuration"
+log_step "  Company: $MPY_USERINIT_USER_COMPANY"
+log_step "  Email: $IKB_SMTP_USER"
+log_step "  Website: $MPY_USERINIT_WEBSITE"
+log_step "  Base URL: $APP_PRIMARY_URL"
+log_step "  Partner root name: $MPY_USERINIT_PARTNER_ROOT_NAME"
 
 bin/sunray-srvr setup-company --name="$MPY_USERINIT_USER_COMPANY" \
     --email=$IKB_SMTP_USER \
-    --website="https://gitlab.com/cmorisse/inouk-sunray-server" \
+    --website="$MPY_USERINIT_WEBSITE" \
     --base-url=$APP_PRIMARY_URL \
     --update-partner \
-    --partner-root-name="SunrayBot"
+    --partner-root-name="$MPY_USERINIT_PARTNER_ROOT_NAME"
 
 # We check if password is set, if not, we will send a signup email
+log_step "Setting up user account"
 if [ -z "$MPY_USERINIT_USER_PASSWORD" ]; then
-    echo "No password set, signup email will be sent to $MPY_USERINIT_USER_EMAIL"
+    log_step "No password set, will send signup email to $MPY_USERINIT_USER_EMAIL"
+    
+    log_step "Creating user without password"
     bin/sunray-srvr setup-user --external-id=$MPY_USERINIT_EXTERNAL_ID \
         --login=$MPY_USERINIT_USER_EMAIL \
         --name="$MPY_USERINIT_USER_NAME"
+    
+    log_step "Sending signup invitation email"
     bin/sunray-srvr send-signup-email --login=$MPY_USERINIT_USER_EMAIL --create-user
+    
+    log_step "Getting signup URL for user"
     bin/sunray-srvr get-signup-url --login=$MPY_USERINIT_USER_EMAIL
 
 else
-    echo "Creating user $MPY_USERINIT_USER_EMAIL from supplied password and TOTP secret."
+    log_step "Creating user $MPY_USERINIT_USER_EMAIL with supplied password"
     bin/sunray-srvr setup-user --external-id=$MPY_USERINIT_EXTERNAL_ID \
         --login=$MPY_USERINIT_USER_EMAIL \
         --name="$MPY_USERINIT_USER_NAME" \
         --password=$MPY_USERINIT_USER_PASSWORD
 fi
+
+log_step "Sunray database initialization completed"
